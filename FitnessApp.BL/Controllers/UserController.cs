3bd7d4f492ce8d48c7566e575d36a107445d0ec8
D3N1s1s <kozyrevd2001@ukr.net>
2020-07-08 23:07:31 +0300

    Add controller and UI #1

--- FitnessApp.BL/Controllers/UserController.cs
+++ FitnessApp.BL/Controllers/UserController.cs
@@ -1,21 +1,62 @@
 ﻿using FitnessApp.BL.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 
 
 namespace FitnessApp.BL.Controllers
 {
+    /// <summary>
+    /// User's controller.
+    /// </summary>
     public class UserController
     {
+        /// <summary>
+        /// App user.
+        /// </summary>
         public User User { get; }
-        public UserController(User user)
+
+        /// <summary>
+        /// Creating new user controller.
+        /// </summary>
+        /// <param name="user"></param>
+        public UserController(string userFName, string userLName, string nickname,string userGender,
+            DateTime birthDate, 
+            double weight, double height )
         {
-            User = user ?? throw new ArgumentNullException("User is'nt found",nameof(user));
+            var gender = new UserGender(userGender); 
+            User = new User(userFName, userLName, nickname, birthDate, gender, weight, height);
         }
-        public bool Save()
+        /// <summary>
+        /// Load user's data.
+        /// </summary>
+        /// <returns>User's data or empty file</returns>
+        public UserController()
         {
-            throw new NotImplementedException();
+            var binFormatter = new BinaryFormatter();
+            using (var binFile = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                if (binFormatter.Deserialize(binFile) is User user)
+                {
+                    User = user;
+                }
+                //TODO шо робыть если пользователя для сериализации не нашлось? ??Вернуть форму с предупреждением либо пустой бинарник либо предложить создать нового
+            }
         }
+
+        /// <summary>
+        /// Save user's data.
+        /// </summary>
+        public void SaveUser()
+        {
+            var binFormatter = new BinaryFormatter();
+            using (var binFile = new FileStream("users.dat", FileMode.OpenOrCreate))
+            {
+                binFormatter.Serialize(binFile, User);
+            }
+        }
+        
     }
 }
---
+++ FitnessApp.UI/Program.cs
@@ -0,0 +1,53 @@
+﻿using FitnessApp.BL.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello fucking fat animal");
+            Console.WriteLine("My name is FAT_KILLER_SUPER_APP");
+            Console.WriteLine("And now you will be track your weight!");
+
+            Console.WriteLine("Enter your First Name, baby: ");
+            var firstName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Last Name, baby: ");
+            var lastName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Nickname, baby: ");
+            var nickname = Console.ReadLine();
+
+            Console.WriteLine("Enter your Gender: ");
+            var gender = Console.ReadLine();
+            
+            Console.WriteLine("Enter the date birth: ");
+            var birthDate = DateTime.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your weight: ");
+            var weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height: ");
+            var height = double.Parse(Console.ReadLine());
+
+            var userController = new UserController(firstName,lastName,nickname,gender,birthDate,weight,height);
+            userController.SaveUser();
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+        }
+    }
+}
