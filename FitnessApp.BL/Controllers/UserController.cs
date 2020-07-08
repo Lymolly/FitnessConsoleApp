bbea6769bbf25547e2b1bad421df3f93152c3261
D3N1s1s <kozyrevd2001@ukr.net>
2020-07-08 16:49:49 +0300

    commit#1

---
+++ FitnessApp.BL/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using FitnessApp.BL.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace FitnessApp.BL.Controllers
+{
+    public class UserController
+    {
+        public User User { get; }
+        public UserController(User user)
+        {
+            User = user ?? throw new ArgumentNullException("User is'nt found",nameof(user));
+        }
+        public bool Save()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
